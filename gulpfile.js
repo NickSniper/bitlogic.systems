cf1d916b3ca99afe79e6a4464b64716a430b925c
NickSniper <nicksniper2@gmail.com>
2023-06-29 09:10:21 +0300

    !!! gulp dist upgraded, dev removed

--- gulpfile.js
+++ gulpfile.js
@@ -37,14 +37,6 @@ const paths = {
         img: './dist/assets/img',
         vendor: './dist/vendor'
     },
-    dev: {
-        base: './html&css/',
-        css: './html&css/css',
-        html: './html&css/html',
-        assets: './html&css/assets',
-        img: './html&css/assets/img',
-        vendor: './html&css/vendor'
-    },
     base: {
         base: './',
         node: './node_modules'
@@ -159,15 +151,6 @@ gulp.task('serve', gulp.series('scss', 'html', 'index', 'assets', 'vendor', func
     gulp.watch([paths.src.vendor], gulp.series('vendor'));
 }));
 
-// Beautify CSS
-gulp.task('beautify:css', function () {
-    return gulp.src([
-        paths.dev.css + '/pixel.css'
-    ])
-        .pipe(cssbeautify())
-        .pipe(gulp.dest(paths.dev.css))
-});
-
 // Minify CSS
 gulp.task('minify:css', function () {
     return gulp.src([
@@ -215,11 +198,6 @@ gulp.task('clean:dist', function () {
     return del([paths.dist.base]);
 });
 
-gulp.task('clean:dev', function () {
-    cache.caches = {};
-    return del([paths.dev.base]);
-});
-
 // Compile and copy scss/css
 gulp.task('copy:dist:css', function () {
     return gulp.src([paths.src.scss + '/custom/**/*.scss', paths.src.scss + '/pixel/**/*.scss', paths.src.scss + '/pixel.scss'])
@@ -231,18 +209,6 @@ gulp.task('copy:dist:css', function () {
         }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.dist.css))
-});
-
-gulp.task('copy:dev:css', function () {
-    return gulp.src([paths.src.scss + '/custom/**/*.scss', paths.src.scss + '/pixel/**/*.scss', paths.src.scss + '/pixel.scss'])
-        .pipe(wait(500))
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['> 1%']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.dev.css))
 });
 
 // Copy Html
@@ -258,18 +224,6 @@ gulp.task('copy:dist:html', function () {
         .pipe(gulp.dest(paths.dist.html));
 });
 
-gulp.task('copy:dev:html', function () {
-    return gulp.src([paths.src.html])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: './src/partials/',
-            context: {
-                environment: 'development'
-            }
-        }))
-        .pipe(gulp.dest(paths.dev.html));
-});
-
 // Copy index
 gulp.task('copy:dist:html:index', function () {
     return gulp.src([paths.src.base + '*.html'])
@@ -283,27 +237,10 @@ gulp.task('copy:dist:html:index', function () {
         .pipe(gulp.dest(paths.dist.base))
 });
 
-gulp.task('copy:dev:html:index', function () {
-    return gulp.src([paths.src.base + '*.html'])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: './src/partials/',
-            context: {
-                environment: 'development'
-            }
-        }))
-        .pipe(gulp.dest(paths.dev.base))
-});
-
 // Copy assets
 gulp.task('copy:dist:assets', function () {
     return gulp.src(paths.src.assets)
         .pipe(gulp.dest(paths.dist.assets))
-});
-
-gulp.task('copy:dev:assets', function () {
-    return gulp.src(paths.src.assets)
-        .pipe(gulp.dest(paths.dev.assets))
 });
 
 gulp.task('copy:dist:image', function () {
@@ -317,21 +254,11 @@ gulp.task('compress:dist:image', function () {
         .pipe(gulp.dest(paths.dist.img));
 });
 
-
-gulp.task('copy:dev:image', function () {
-    return gulp.src(paths.src.img)
-        .pipe(gulp.dest(paths.dev.img))
-});
-
 // Copy node_modules to vendor
 gulp.task('copy:dist:vendor', function () {
-    return gulp.src(npmDist(), { base: paths.src.node_modules })
+    // return gulp.src(npmDist(), { base: paths.src.node_modules })
+    return gulp.src([paths.src.node_modules + '/**/**/*.min.css', paths.src.node_modules + '/**/**/*.woff2']) // copy css & woff2 only
         .pipe(gulp.dest(paths.dist.vendor));
-});
-
-gulp.task('copy:dev:vendor', function () {
-    return gulp.src(npmDist(), { base: paths.src.node_modules })
-        .pipe(gulp.dest(paths.dev.vendor));
 });
 
 gulp.task('dist:concat:js', function () {
@@ -364,7 +291,6 @@ gulp.task('dist:deploy', function () {
 });
 
 
-gulp.task('build:dev', gulp.series('clean:dev', 'copy:dev:css', 'copy:dev:html', 'copy:dev:html:index', 'copy:dev:assets', 'beautify:css', 'copy:dev:vendor'));
 gulp.task('build:dist', gulp.series('clean:dist', 'copy:dist:css', 'copy:dist:html', 'copy:dist:html:index', 'copy:dist:assets', /*'compress:dist:image',*/ 'minify:css', 'minify:html', 'minify:html:index', 'copy:dist:vendor', 'dist:concat:js', 'dist:deploy'));
 
 // Default
