dbbea3648f1c6d2bc3f457f6597f359cfde38320
NickSniper <nicksniper2@gmail.com>
2023-07-11 02:52:52 +0300

    !!! settings is working

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -7,17 +7,5 @@ $(document).ready(function () {
         handle: ".modal-content"
     });
 
-    // fadeRangeOnInput = function (input, range) {
-    //     console.log(input, range);
-    //     const fadeRangeOut = document.getElementById('#fadeRangeOut');
-    //     fadeRangeOut.textContent = input.value;
-    //     const elements = document.querySelectorAll('.image-fade-left');
-
-    //     elements.forEach(function (element) {
-    //         const afterElement = window.getComputedStyle(element, '::after');
-    //         afterElement.width = '50%';
-    //     });
-    // }
-
 });
 
