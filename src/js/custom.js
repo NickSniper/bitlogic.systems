d219944ad220b527bf0729bcfe4a5565f6be3d3e
NickSniper <nicksniper2@gmail.com>
2023-07-24 18:43:16 +0300

    ! width fixed

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,6 @@
 // <!-- Copyright © 2011-2023 bitLogic.systems -->
 
-$(document).ready(function () {
+$(function () {
 
     // Modal draggable
     $('.modal').find('.modal-dialog:first').draggable({
